493a6c1fca00fdb672ea8e11a023326733e8a048
Ctkadlec <chase.kadlec@kellogg.northwestern.edu>
2020-02-23 21:27:14 +0000

    removing table, adding grid

--- scratch.rb
+++ scratch.rb
@@ -1,13 +1,43 @@
-     current_temperature = forecast["currently"]["temperature"]
-conditions = forecast["currently"]["summary"]
-puts "In Evanston, it is currently #{current_temperature} and #{conditions}"
-
-        # high_temperature = forecast["daily"]["data"][0]["temperatureHigh"]
-        # puts high_temperature
-        # puts high_temperature = forecast["daily"]["data"][1]["temperatureHigh"]
-
-for day in forecast["daily"]["data"]
-    puts "A high temperature of #{day["temperatureHigh"]} and #{day["summary"]}"
-end 
-
-    @forecast = ForecastIO.forecast(@lat, @long).to_hash
+<div class="table-responsive">
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Title</th>
+      <th scope="col">Description</th>
+      <th scope="col">Source</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td><a href="<%=@article1url%>"><%=@article1title%></a></td>
+      <td><%=@article1des%></td>
+      <td><%=@article1source%></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+     <td><a href="<%=@article2url%>"><%=@article2title%></a></td>
+      <td><%=@article2des%></td>
+      <td><%=@article2source%></td>
+    </tr>
+   <tr>
+      <th scope="row">3</th>
+    <td><a href="<%=@article3url%>"><%=@article3title%></a></td>
+      <td><%=@article3des%></td>
+      <td><%=@article3source%></td>
+    </tr>
+     <tr>
+      <th scope="row">4</th>
+     <td><a href="<%=@article4url%>"><%=@article4title%></a></td>
+      <td><%=@article4des%></td>
+      <td><%=@article4source%></td>
+    </tr>
+     <tr>
+      <th scope="row">5</th>
+     <td><a href="<%=@article5url%>"><%=@article5title%></a></td>
+      <td><%=@article5des%></td>
+      <td><%=@article5source%></td>
+    </tr>
+  </tbody>
+</table>
